99be03bd666abec434ca3ec5950cd1069225a796
licheng173 <cl3570@nyu.edu>
2016-09-08 13:53:47 -0400

    j

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,32 +1,3 @@
-//worst case use hashmap but slow
-public class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int length = s.length();
-        int count = 0;
-        int max = 0;
-        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
-        for(int i = 0; i < (length- max); i ++){
-            int j = i;
-           
-           while(!map.containsKey(s.charAt(j))) {
-                map.put(s.charAt(j), j);
-                count++;
-                if(j == length-1){
-                    break;
-                }
-                j++;
-           }
-           map.clear();
-           if(max <= count){
-               max = count;
-           }
-            count = 0;
-          
-        }
-        return max;
-    }
-}
-
 //second best use set easier than hashmap
 import java.util.HashSet;
 import java.util.Set;
@@ -52,37 +23,32 @@ public class Solution {
         System.out.print(t);
     }
 }
-// best solution but not simple, use array to impliment hashmap function, most efficient but not easy to read
+
+
+//hashmao && two pointers
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int lastIndices[] = new int[256];
-        for(int i = 0; i<256; i++){
-            lastIndices[i] = -1;
-        }
-        
-        int maxLen = 0;
-        int curLen = 0;
+        int length = s.length();
+        if(length == 0 || s == null) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        char[] charArray = s.toCharArray();
         int start = 0;
-        int bestStart = 0;
-        for(int i = 0; i<s.length(); i++){
-            char cur = s.charAt(i);
-            if(lastIndices[cur]  < start){
-                lastIndices[cur] = i;
-                curLen++;
+        int max = Integer.MIN_VALUE;
+        //int end = length - 1;
+        int i = start;
+        while(i < length) {
+            char p = charArray[i];
+            if(map.containsKey(p)) {
+                if(map.get(p) >= start) {
+                    start = map.get(p);
+                    start++;
+                }
             }
-            else{
-                int lastIndex = lastIndices[cur];
-                start = lastIndex+1;
-                curLen = i-start+1;
-                lastIndices[cur] = i;
-            }
-            
-            if(curLen > maxLen){
-                maxLen = curLen;
-                bestStart = start;
-            }
+            map.put(p, i);
+            i++;
+            max = Math.max(max, i - start);
+            //System.out.println(start);
         }
-        
-        return maxLen;
+        return max;
     }
 }
