1dd34d2462122e4d0ff37a9181cd4b674bb9058f
licheng173 <cl3570@nyu.edu>
2016-10-05 18:20:13 -0400

    vc

---
+++ 379. Design Phone Directory.java
@@ -0,0 +1,76 @@
+public class PhoneDirectory {
+
+    Set<Integer> used = new HashSet<Integer>();
+    Queue<Integer> available = new LinkedList<Integer>();
+    int max;
+    public PhoneDirectory(int maxNumbers) {
+        max = maxNumbers;
+        for (int i = 0; i < maxNumbers; i++) {
+            available.offer(i);
+        }
+    }
+    
+    public int get() {
+        Integer ret = available.poll();
+        if (ret == null) {
+            return -1;
+        }
+        used.add(ret);
+        return ret;
+    }
+    
+    public boolean check(int number) {
+        if (number >= max || number < 0) {
+            return false;
+        }
+        return !used.contains(number);
+    }
+    
+    public void release(int number) {
+        if (used.remove(number)) {
+            available.offer(number);
+        }
+    }
+}
+
+
+
+//diaodiao
+public class PhoneDirectory {
+
+    BitSet bitset;
+    int max; // max limit allowed
+    int smallestFreeIndex; // current smallest index of the free bit
+
+    public PhoneDirectory(int maxNumbers) {
+        this.bitset = new BitSet(maxNumbers);
+        this.max = maxNumbers;
+    }
+
+    public int get() {
+        // handle bitset fully allocated
+        if(smallestFreeIndex == max) {
+            return -1;
+        }
+        int num = smallestFreeIndex;
+        bitset.set(smallestFreeIndex);
+        //Only scan for the next free bit, from the previously known smallest free index
+        smallestFreeIndex = bitset.nextClearBit(smallestFreeIndex);
+        return num;
+    }
+
+    public boolean check(int number) {
+        return bitset.get(number) == false;
+    }
+
+    public void release(int number) {
+        //handle release of unallocated ones
+        if(bitset.get(number) == false)
+            return;
+        bitset.clear(number);
+        if(number < smallestFreeIndex) {
+            smallestFreeIndex = number;
+        }
+    }
+}
+
